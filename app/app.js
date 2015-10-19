43b7cf02dd5c2dd992122650471df2720b13e735
Rigel St. Pierre <rigel@rigel.co>
2015-10-19 15:06:47 -0600

    remove router temporarly, removed unneeded files

--- app/app.js
+++ app/app.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { render } from 'react-dom'
-import { Router, Route, Link } from 'react-router';
 import ApplicationWrapper from "./application_wrapper";
 
 require('./stylesheets/style.scss');
 
-render((
-  <Router>
-    <Route path="/" component={ ApplicationWrapper }>
-    </Route>
-  </Router>
-), document.getElementById("app"))
+render(
+  <ApplicationWrapper />,
+  document.getElementById("app")
+)
